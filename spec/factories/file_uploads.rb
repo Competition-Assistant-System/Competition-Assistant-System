6b70e1cc8d7e87fdfaecaf707369f9f08ccf5ace
Jack Hu <spcreply@126.com>
2020-11-27 21:13:34 +0800

    add model tests

--- spec/factories/file_uploads.rb
+++ spec/factories/file_uploads.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :file_upload do
-    file_size { 0 }
+    file_size { "" }
     file_path { "MyString" }
   end
 end
---
+++ test/models/score_result_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ScoreResultTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
