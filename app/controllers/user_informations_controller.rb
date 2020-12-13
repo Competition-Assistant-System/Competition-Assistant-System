f0d042084fa802d2aa4b02d193259fdf27f1dd4c
xuefeng <xuefeng16@mails.ucas.ac.cn>
2020-12-13 12:28:58 +0800

    Add shared error_msg && UserInfo Update

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -1,12 +1,22 @@
 class UserInformationsController < ApplicationController
 
-
   def edit
     @info = UserInformation.find(params[:id])
   end
 
-  def create
-
+  def update
+    @info = UserInformation.find(params[:id])
+    if @info.update(info_params)
+      flash[:success] = "Profile updated"
+      redirect_to @info # TODO:
+    else
+      render 'edit'
+    end
   end
+
+  private
+    def info_params
+      params.require(:user_information).permit(:nickname, :school)
+    end
 
 end
--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -1,3 +1,8 @@
 class UserInformation < ApplicationRecord
     belongs_to :user
+
+    validates :user_id, presence: true
+    validates :nickname, presence: true, length: { maximum: 50 }
+    validates :school, presence: true, length: { maximum: 100 }
+
 end
