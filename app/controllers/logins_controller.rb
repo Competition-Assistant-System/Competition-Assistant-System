249064e7f2b21e1408d99c4cbb7ffe407e13c96b
JamesAslan <34978717+JamesAslan@users.noreply.github.com>
2021-01-07 20:14:56 +0800

    11

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -3,7 +3,7 @@ class LoginsController < ApplicationController
         @user = User.new
       end
 
-      def register
+    def register
         @user = User.new(user_params)
         if @user.save
           redirect_to user_url(@user)
@@ -11,13 +11,11 @@ class LoginsController < ApplicationController
           @user.errors.full_messages
           render 'new'
         end
-        
-      end
+    end
     
-      private
+    private
     
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,25 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
-      end
+    end
     
-      def new
+    def new
         @user = User.new
-      end
+    end
 
-      def create
+    def create
         @user = User.new(user_params)
         if @user.save
-          redirect_to user_url(@user)
+            redirect_to user_url(@user)
         else
-          @user.errors.full_messages
-          render 'new'
-        end
-        
-      end
+            @user.errors.full_messages
+            render 'new'
+        end  
+    end
     
-      private
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
     
-        def user_params
-            params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    
-        end
 end
