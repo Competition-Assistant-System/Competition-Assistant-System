23a9c03947f3ecb848d05c50ee61d743c30a1862
xuefeng <xuefeng16@mails.ucas.ac.cn>
2021-01-07 21:34:54 +0800

    Add final_result controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'final_results/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'examples#index'
 
   resources :user_informations, only: [:edit, :update, :show]
+  resources :final_results, only: [:index]
 
   resources :examples, only: :index do
     get :buttons, :cards, :utilities_color, :utilities_border,
