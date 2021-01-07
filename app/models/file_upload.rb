b6d2c2061039e8abd4e48867908edaf8b6d802b0
Jack Hu <spcreply@126.com>
2021-01-07 13:58:33 +0800

    Add FileUpload model function

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -1,7 +1,6 @@
 class FileUpload < ApplicationRecord
     belongs_to :user
 
-    File_extname = [".rar",".7z",".zip"]
     File_target = "/public/files/"
     validates :file_name, presence: true
     validates :file_md5, presence: true
@@ -18,5 +17,24 @@ class FileUpload < ApplicationRecord
         if File.exist?(file_path)
             File.delete(file_path)
         end
+    end
+
+    def upload_file file
+        if File.exist?(file)
+            ext_name = File.extname(file)
+            self.file_name = File.basename(file)
+            # clac md5
+            self.file_md5 = File.basename(file, ext_name) + "_" + clac_file_md5(file) + ext_name
+            # clac size
+            self.file_size = File.size(file)
+            # change name
+            file_path = File.join(Rails.root,FileUpload::File_target,self.file_md5)
+            # rename(move)
+            File.rename(file, file_path)
+        end
+    end
+    
+    def clac_file_md5(file)
+        Digest::MD5.hexdigest(File.open(file, "rb") { |f| "#{f.read}" })
     end
 end
