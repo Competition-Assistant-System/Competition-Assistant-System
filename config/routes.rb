13e950c4992929f597112f41b9835279d3b91b62
JamesAslan <34978717+JamesAslan@users.noreply.github.com>
2021-01-07 20:33:51 +0800

    register

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
   resources :user_informations, only: [:edit, :update]
   resources :score_ranking, only: [:index]
+  resources :logins
   
   resources :examples, only: :index do
     get :buttons, :cards, :utilities_color, :utilities_border,
