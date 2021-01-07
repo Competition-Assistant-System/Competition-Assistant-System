6015a5ea41d0b5e046355d4f4561acdd6fc4fa7f
Chenji <hanchenji@mails.ucas.ac.cn>
2021-01-07 15:30:21 +0800

    ready to merge from the main branch

--- app/controllers/submit_files_controller.rb
+++ app/controllers/submit_files_controller.rb
@@ -1,25 +1,20 @@
 class SubmitFilesController < ApplicationController
 
-  def submit
-    @uploaded_file = UploadedFile.new(params[:uploaded_file])
-    unless request.get?
-        @uploaded_file.uploaded_file_url = uploadfile(@uploaded_file.uploaded_file_url)
-        @uploaded_file.save
-    end
+  def index
   end
 
-  def uploadfile(file)
-    if !file.original_filename.empty?
-      @filename = file.original_filename
-      #设置目录路径，如果目录不存在，生成新目录
-      FileUtils.mkdir("#{Rails.root}/public/upload") unless File.exist?("#{Rails.root}/public/upload")
-      #写入文件
-      ##wb 表示通过二进制方式写，可以保证文件不损坏
-      File.open("#{Rails.root}/public/upload/#{@filename}", "wb") do |f|
-        f.write(file.read)
-      end
-      return @filename
-    end
+  def submit_scores
+      user_id = 1
+      @user = User.find_by_id(user_id)
+      @user_score = 0 
+      
+  end
+  
+  def submit_files
+      user_id = 1
+      @user = User.find_by_id(user_id)
+      @user_uploaded_file = 0 
+     
   end
 
 end
