61f46a629482d2c556a58953dd5b5276d9138847
ZhangZifei <1773908404@qq.com>
2021-01-07 23:48:55 +0800

    add approve/disapprove that cannot work && index.html.erb

--- app/controllers/admin/user_confirm_controller.rb
+++ app/controllers/admin/user_confirm_controller.rb
@@ -1,4 +1,24 @@
 class Admin::UserConfirmController < ApplicationController
-  def index
-  end
+    def index
+        @users = User.all
+        @users_unaudit = @users # .select{ |user| user.@application.not_audit_yet?}
+    end
+
+    # approve the application
+    def approve(user)
+        # user.grant.set_participant
+        # user.@application.approve!
+        user.save
+    rescue => exception
+        Logger.new(exception)
+    end
+
+    # disapprove the application
+    def disapprove(user)
+        # user.@application.disapprove!
+        # user.grant.set_common
+        user.save
+    rescue => exception
+        Logger.new(exception)
+    end
 end
