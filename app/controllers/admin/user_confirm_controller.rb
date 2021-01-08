52bbeec9127f0195b6a330cb884be01d6a699d9a
ZhangZifei <1773908404@qq.com>
2021-01-08 16:42:00 +0800

    user confirm: use grant_id to audit

--- app/controllers/admin/user_confirm_controller.rb
+++ app/controllers/admin/user_confirm_controller.rb
@@ -1,13 +1,21 @@
 class Admin::UserConfirmController < ApplicationController
     def index
+      # grant_id:
+      # 0: admin
+      # 1: normal participant
+      # 2: not audited
+      # 3: not passed
         @users = User.all
-        @users_unaudit = @users # .select{ |user| user.@application.not_audit_yet?}
+        @users_unaudit = @users.select{ |user| user.grant_id==2}
     end
 
     # approve the application
     def approve(user)
-        # user.grant.set_participant
-        # user.@application.approve!
+      if (user.grant_id!=0) then
+        user.grant_id = 1
+      else
+        Logger.new("Admin should not applicate")
+      end
         user.save
     rescue => exception
         Logger.new(exception)
@@ -15,8 +23,11 @@ class Admin::UserConfirmController < ApplicationController
 
     # disapprove the application
     def disapprove(user)
-        # user.@application.disapprove!
-        # user.grant.set_common
+        if(user.grant_id!=0) then
+            user.grant_id = 1
+        else
+            Logger.new("Admin should not applicate")
+        end
         user.save
     rescue => exception
         Logger.new(exception)
