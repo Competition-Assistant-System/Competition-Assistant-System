528e648a9419ab2f58d27dce4e2af1940a279f59
Jack Hu <spcreply@126.com>
2021-01-08 19:43:17 +0800

    Fix ranking show

--- app/controllers/score_ranking_controller.rb
+++ app/controllers/score_ranking_controller.rb
@@ -3,6 +3,7 @@ class ScoreRankingController < ApplicationController
         @rank = User.joins(score_results: :score_base)
             .group("users.id")
             .order("sum(score_results.score_result * score_bases.weight) desc")
+            .where(:grant_id=>1)
         @score_item = ScoreBase.all.order(:id)
     end
 end
